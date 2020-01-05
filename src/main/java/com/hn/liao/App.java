b0837dca65404f8985181996a05ae6686a4aac26
Administrator <jnliao116@163.com>
2020-01-05 17:32:44 +0800

    使用jstat观察程序的运行状态

--- src/main/java/com/hn/liao/App.java
+++ src/main/java/com/hn/liao/App.java
@@ -5,10 +5,10 @@ package com.hn.liao;
  */
 public class App {
     public static void main(String[] args) {
-        demoBigObjDirectIntoOld();
+        demoForYoungGcOften();
     }
 
-    public static void demoForYoungGc(){
+    public static void demoForYoungGc() {
         byte[] array1 = new byte[1024 * 1024];
         array1 = new byte[1024 * 1024];
         array1 = new byte[1024 * 1024];
@@ -17,7 +17,7 @@ public class App {
         byte[] array2 = new byte[2 * 1024 * 1024];
     }
 
-    public static void demoForYoungGc2(){
+    public static void demoForYoungGc2() {
         byte[] array1 = new byte[1024 * 1024];
         array1 = new byte[1024 * 1024];
         array1 = new byte[1024 * 1024];
@@ -26,7 +26,37 @@ public class App {
         byte[] array2 = new byte[4 * 1024 * 1024];
     }
 
-    public static void demoBigObjDirectIntoOld(){
+    public static void demoBigObjDirectIntoOld() {
         byte[] array1 = new byte[2 * 1024 * 1024];
     }
+
+    public static void demoForYoungGcOften() {
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (true){
+            loadData();
+        }
+
+    }
+
+    private static void loadData(){
+        byte[] data = null;
+        for(int i=0;i<50;i++){
+            data  =new byte[100 *1024];
+        }
+        data = null;
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
