cad97792c68e36baf2cbc8baa3a59f1515febc94
liaojinneng <jinneng.liao@zhangmen.com>
2020-04-05 20:18:16 +0800

    jvm 堆内存溢出

--- src/main/java/com/hn/liao/App.java
+++ src/main/java/com/hn/liao/App.java
@@ -1,13 +1,20 @@
 package com.hn.liao;
 
+import java.util.ArrayList;
+
 /**
  * @author jnliao
  */
 public class App {
     public static void main(String[] args) {
-        demoForYoungGcOften();
+        //demoForYoungGcOften();
+
+        demoForHeapOOM();
     }
 
+    /**
+     * 模拟 youngGc
+     */
     public static void demoForYoungGc() {
         byte[] array1 = new byte[1024 * 1024];
         array1 = new byte[1024 * 1024];
@@ -26,6 +33,9 @@ public class App {
         byte[] array2 = new byte[4 * 1024 * 1024];
     }
 
+    /**
+     * 模拟使用cms时，大对象进入老年代
+     */
     public static void demoBigObjDirectIntoOld() {
         byte[] array1 = new byte[2 * 1024 * 1024];
     }
@@ -54,6 +64,18 @@ public class App {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 模拟堆内存溢出 360145
+     */
+    public static void demoForHeapOOM(){
+        long count = 0;
+        ArrayList<Object> arrayList = new ArrayList<>();
+        while(true){
+            arrayList.add(new Object());
+            System.out.println("创建第【"+(++count)+"】个对象");
         }
     }
 
